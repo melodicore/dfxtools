7a2e445bab7c90dba307fc3b35408d2751418504
melodicore <datafox@datafox.me>
2025-05-03 15:38:55 +0300

    forgot to add values build file

---
+++ values/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("dfxtools.maven-library-convention")
+    id("dfxtools.dokka-convention")
+}
+
+version = libs.versions.handles.get()
+
+dependencies {
+    implementation(project(":handles"))
+    implementation(project(":invalidation"))
+    implementation(project(":utils"))
+    implementation(libs.logging)
+    implementation(libs.slf4j.api)
+    compileClasspath(libs.kotlin.reflect)
+    testRuntimeClasspath(libs.log4j)
+}
+
+mavenPublishing {
+    coordinates(group.toString(), name, version.toString())
+    pom {
+        name = "DFXTools Values"
+        description = "Mutable numbers with an extensible modifier system"
+    }
+}
