caf4f9ab8c35c6aaccca8dd9b940724d9259b53b
melodicore <datafox@datafox.me>
2025-05-31 13:00:10 +0300

    Invalidation more clear error message

--- invalidation/src/main/kotlin/me/datafox/dfxtools/invalidation/internal/Strings.kt
+++ invalidation/src/main/kotlin/me/datafox/dfxtools/invalidation/internal/Strings.kt
@@ -12,5 +12,6 @@ internal object Strings {
     fun selfDependency(observer: Observer) = "$observer cannot observe itself"
 
     fun cyclicDependency(current: Observable, original: Observable, owner: Observer) =
-        "$original cannot observe $owner because it would cause a cyclic dependency ($owner observes $current)"
+        "$original cannot observe $owner because it would cause a cyclic dependency " +
+                "($owner observes $current which either directly or indirectly observes $original)"
 }
